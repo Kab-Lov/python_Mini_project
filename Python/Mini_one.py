951671655c79efc81a4d5b0e8f7ee1d9edb6e5c5
kab-lov <neaneazin2@gmail.com>
2026-02-27 15:17:21 +0700

    New code

--- Python/Mini_one.py
+++ Python/Mini_one.py
@@ -3,4 +3,17 @@ import random
 def roll():
     min_value = 1
     max_value = 6
-    roll = random.randint
+    roll = random.randint(min_value, max_value)
+
+    return roll
+while True:
+    players = input("Input the number from 2-4: ")
+    if players.isdigit():
+        players =int(players)
+        if 2 <= players <= 4:
+            print("Must be 2 to 4")
+            break
+    else:
+        print("Invalid, try again! ")
+
+print(players)
